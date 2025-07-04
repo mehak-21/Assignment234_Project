3c60079919684a9dfbbec01f17afca5aee5bd24a
Mehak Abdul Samad <f20232661020@umt.edu.pk>
2025-07-03 21:35:53 -0700

    Update admin.py

--- resume_builder/admin.py
+++ resume_builder/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import WorkExperience, Education, Project, Certification, Award, Language, TechnicalSkill
 
-# Register your models here.
+# Register all models to appear in admin panel
+admin.site.register(WorkExperience)
+admin.site.register(Education)
+admin.site.register(Project)
+admin.site.register(Certification)
+admin.site.register(Award)
+admin.site.register(Language)
+admin.site.register(TechnicalSkill)
