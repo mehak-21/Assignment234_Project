a424d4d3e10269e34e175527490a6d97240f9812
Mehak Abdul Samad <f20232661020@umt.edu.pk>
2025-07-03 21:31:31 -0700

    Update views.py

--- interviews/views.py
+++ interviews/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from resume_builder.models import WorkExperience
+from resume_builder.forms import WorkExperienceForm
 
-# Create your views here.
+@login_required
+def create_workexperience(request):
+    if request.method == 'POST':
+        form = WorkExperienceForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.user = request.user
+            obj.save()
+            return redirect('workexperience_list')
+    else:
+        form = WorkExperienceForm()
+    return render(request, 'resume_builder/workexperience_form.html', {'form': form})
+
+@login_required
+def workexperience_list(request):
+    items = WorkExperience.objects.filter(user=request.user)
+    return render(request, 'resume_builder/workexperience_list.html', {'items': items})
+
+@login_required
+def workexperience_detail(request, pk):
+    item = get_object_or_404(WorkExperience, pk=pk, user=request.user)
+    return render(request, 'resume_builder/workexperience_detail.html', {'item': item})
+
+@login_required
+def update_workexperience(request, pk):
+    item = get_object_or_404(WorkExperience, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = WorkExperienceForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('workexperience_list')
+    else:
+        form = WorkExperienceForm(instance=item)
+    return render(request, 'resume_builder/workexperience_form.html', {'form': form})
+
+@login_required
+def delete_workexperience(request, pk):
+    item = get_object_or_404(WorkExperience, pk=pk, user=request.user)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('workexperience_list')
+    return render(request, 'resume_builder/workexperience_confirm_delete.html', {'item': item})
