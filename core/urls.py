2a52df2303493ecd745581162d38c18310f698b3
Mehak Abdul Samad <f20232661020@umt.edu.pk>
2025-07-03 21:32:25 -0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
+URL configuration for core project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path,include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
@@ -26,7 +42,6 @@ web_patterns = [
     path('web/', include('resume_builder.web.urls')),
     path('web/', include('accounts.web.urls')),
     path('', include('dashboard.urls')),
-
 ]
 apis_patterns = [
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
@@ -41,5 +56,4 @@ apis_patterns = [
 urlpatterns = web_patterns + apis_patterns
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
